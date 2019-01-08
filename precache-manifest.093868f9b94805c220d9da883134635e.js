2501d8e20fff82bdeeaa3aff39813499ac3360aa
Dakota Daniel Maker <dakota.maker@jda.com>
2019-01-07 19:24:15 -0600

    Updates

---
+++ precache-manifest.093868f9b94805c220d9da883134635e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e1701c745545a233f9da55c96e884a4f",
+    "url": "/resume-site/static/media/logo.e1701c74.png"
+  },
+  {
+    "revision": "ad97c26fb2433c6a9ce7833d77e38d2c",
+    "url": "/resume-site/static/media/jda.ad97c26f.png"
+  },
+  {
+    "revision": "8da5e87e9ca31c282152b6372c96b901",
+    "url": "/resume-site/static/media/UWM.8da5e87e.png"
+  },
+  {
+    "revision": "339559fe91be784efbae831334234bdb",
+    "url": "/resume-site/static/media/Panther.339559fe.png"
+  },
+  {
+    "revision": "4ba565165f614f332102",
+    "url": "/resume-site/static/js/runtime~main.4ba56516.js"
+  },
+  {
+    "revision": "a279a56ec99fb82f83e6",
+    "url": "/resume-site/static/js/main.a279a56e.chunk.js"
+  },
+  {
+    "revision": "fa8c6edccaede0734f73",
+    "url": "/resume-site/static/js/1.fa8c6edc.chunk.js"
+  },
+  {
+    "revision": "a279a56ec99fb82f83e6",
+    "url": "/resume-site/static/css/main.31603554.chunk.css"
+  },
+  {
+    "revision": "f89feab92c44a62eb492453667aad742",
+    "url": "/resume-site/index.html"
+  }
+];
